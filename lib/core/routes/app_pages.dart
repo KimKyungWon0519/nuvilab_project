bcc122882733b3d4472abed2cfe8839a0f1475d5
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 17:41:15 +0900

    FEAT: TimePage 추가

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -4,7 +4,7 @@ import 'package:nuvilab_project/core/routes/app_routes.dart';
 final class AppPages {
   const AppPages._();
 
-  static final String _initialLocation = '';
+  static final String _initialLocation = AppRoutes.time.path;
 
   static final GoRouter routeConfig = GoRouter(
     routes: AppRoutes.routes,
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,7 +1,15 @@
 import 'package:go_router/go_router.dart';
+import 'package:nuvilab_project/presentation/pages/time_page/time_page.dart';
 
 final class AppRoutes {
   const AppRoutes._();
 
-  static final List<RouteBase> routes = [];
+  static final GoRoute time = GoRoute(
+    path: '/time',
+    builder: (context, state) => TimePage(),
+  );
+
+  static final List<RouteBase> routes = [
+    time,
+  ];
 }
