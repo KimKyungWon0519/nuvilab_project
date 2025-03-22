1683d0dbcf1b070c729328d5666b1abc686f24fd
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 13:42:12 +0900

    FEAT: 미세먼지 데이터 가져오는 기능 추가

--- lib/data/fine_dust_api_client.dart
+++ lib/data/fine_dust_api_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:nuvilab_project/core/constants/api_constant.dart';
 
 class FineDustApiClient {
   late final Dio _dio;
@@ -6,4 +7,14 @@ class FineDustApiClient {
   FineDustApiClient({
     required Dio dio,
   }) : _dio = dio;
+
+  getAveragePM10() async {
+    return _dio.get(
+      getMesuringList,
+      queryParameters: {
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+      },
+    );
+  }
 }
