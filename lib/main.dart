c3e9a0b265c5117653418fa6471605a53d197a40
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 17:42:07 +0900

    FEAT: 라우터 클래스 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:nuvilab_project/core/constants/api_constant.dart';
-import 'package:nuvilab_project/core/network/retry_interceptor.dart';
-import 'package:nuvilab_project/data/service/fine_dust_api_client.dart';
+import 'package:nuvilab_project/core/routes/app_pages.dart';
 
 void main() {
-  print('baseURL:$baseURL, serviceKey : $serviceKey');
-
-  Dio dio = Dio(
-    BaseOptions(
-      baseUrl: baseURL,
-      queryParameters: {'serviceKey': serviceKey, 'returnType': 'json'},
-    ),
-  );
-
-  FineDustApiClient(dio: dio).getAveragePM10();
-
   runApp(const MainApp());
 }
 
@@ -24,12 +10,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: AppPages.routeConfig,
     );
   }
 }
