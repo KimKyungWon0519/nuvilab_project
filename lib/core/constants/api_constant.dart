9e9d86f5e665f2ceafc936ba6b966e598d1da8c1
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 23:30:19 +0900

    FEAT: 호출 함수 변경

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -1,4 +1,4 @@
 const String baseURL = String.fromEnvironment('BASE_URL');
 const String serviceKey = String.fromEnvironment('SERVICE_KEY');
 
-const String getMesuringList = '/getCtprvnMesureLIst';
+const String getMesuringListPath = '/getCtprvnMesureLIst';
--- lib/data/repositoies/fine_dust_repository_impl.dart
+++ lib/data/repositoies/fine_dust_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:nuvilab_project/data/model/get_mesuring_list_param.dart';
 import 'package:nuvilab_project/data/model/response_result.dart';
 import 'package:nuvilab_project/data/service/fine_dust_api_client.dart';
 import 'package:nuvilab_project/domain/model/fine_dust_by_city.dart';
@@ -12,11 +13,28 @@ class FineDustRepositoryImpl implements FineDustRepository {
 
   @override
   Future<List<FineDustByCity>> getFineDustByCities() async {
-    Future<ResponseResult> getAveragePM10 = _fineDustApiClient.getAveragePM10();
-    Future<ResponseResult> getAveragePM25 = _fineDustApiClient.getAveragePM25();
+    Future<ResponseResult> getAveragePM10 = _fineDustApiClient.getMesuringList(
+      GetMesuringListParam(
+        itemCode: 'PM10',
+        dataGubun: 'HOUR',
+        numOfRows: 24,
+      ),
+    );
+    Future<ResponseResult> getAveragePM25 = _fineDustApiClient.getMesuringList(
+      GetMesuringListParam(
+        itemCode: 'PM25',
+        dataGubun: 'HOUR',
+        numOfRows: 24,
+      ),
+    );
 
     await Future.wait([getAveragePM10, getAveragePM25]).then(
-      (value) {},
+      (value) {
+        ResponseResult averagePM10 = value[0];
+        ResponseResult averagePM25 = value[1];
+
+        print(averagePM10.header);
+      },
     );
 
     return [];
