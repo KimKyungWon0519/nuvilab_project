53e6de598adae379ff8afd091918352e2f327f86
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 22:18:30 +0900

    FEAT: 페이지 이름 변경

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -4,7 +4,7 @@ import 'package:nuvilab_project/core/routes/app_routes.dart';
 final class AppPages {
   const AppPages._();
 
-  static final String _initialLocation = AppRoutes.time.path;
+  static final String _initialLocation = AppRoutes.fineDustByTime.path;
 
   static final GoRouter routeConfig = GoRouter(
     routes: AppRoutes.routes,
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,15 +1,15 @@
 import 'package:go_router/go_router.dart';
-import 'package:nuvilab_project/presentation/pages/time_page/time_page.dart';
+import 'package:nuvilab_project/presentation/pages/fine_dust_by_time/fine_dust_by_time.dart';
 
 final class AppRoutes {
   const AppRoutes._();
 
-  static final GoRoute time = GoRoute(
-    path: '/time',
+  static final GoRoute fineDustByTime = GoRoute(
+    path: '/fine_dust_by_time',
     builder: (context, state) => TimePage(),
   );
 
   static final List<RouteBase> routes = [
-    time,
+    fineDustByTime,
   ];
 }
