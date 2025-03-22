a41cf0d650e1cd2d794f6fa87ee3bc23f14432b7
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-23 03:08:58 +0900

    FEAT: 미세먼지 레포지토리 맴버변수 추가 및 초기 데이터 호출

--- lib/presentation/riverpods/fine_dust_by_time_notifier.dart
+++ lib/presentation/riverpods/fine_dust_by_time_notifier.dart
@@ -1,12 +1,23 @@
+import 'package:get_it/get_it.dart';
 import 'package:nuvilab_project/domain/model/mesuring_fine_dust.dart';
+import 'package:nuvilab_project/domain/repositoies/fine_dust_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'fine_dust_by_time_notifier.g.dart';
 
 @riverpod
 class FineDustByTimeNotifier extends _$FineDustByTimeNotifier {
+  late final FineDustRepository _fineDustRepository;
+
+  FineDustByTimeNotifier({
+    FineDustRepository? fineDustRepository,
+  }) : _fineDustRepository =
+            fineDustRepository ?? GetIt.I<FineDustRepository>();
+
   @override
   List<MesuringFineDust> build() {
+    _fineDustRepository.getFineDustByCities().then((value) => state = value);
+
     return [];
   }
 }
