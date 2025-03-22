0c6df0b03ba7e978d344afddc4dceae4cd175c87
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 17:44:52 +0900

    FEAT: Appbar에 현재 날짜 표시

--- lib/presentation/pages/time_page/time_page.dart
+++ lib/presentation/pages/time_page/time_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TimePage extends StatelessWidget {
   const TimePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          DateFormat('yyyy-MM-dd').format(
+            DateTime.now(),
+          ),
+        ),
+      ),
+    );
   }
 }
