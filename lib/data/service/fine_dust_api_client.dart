9e55f7b5a96c7e95bb82d18dc6586fcd31589e68
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 17:19:24 +0900

    FEAT: 성공 및 실패 로그 추가

--- lib/data/service/fine_dust_api_client.dart
+++ lib/data/service/fine_dust_api_client.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:logger/web.dart';
 import 'package:nuvilab_project/core/constants/api_constant.dart';
+import 'package:nuvilab_project/core/network/retry_interceptor.dart';
 import 'package:nuvilab_project/data/model/response_result.dart';
 
 class FineDustApiClient {
@@ -7,16 +9,34 @@ class FineDustApiClient {
 
   FineDustApiClient({
     required Dio dio,
-  }) : _dio = dio;
+  }) : _dio = dio {
+    _dio.interceptors.add(RetryInterceptor(dio: _dio));
+  }
 
   Future<ResponseResult> getAveragePM10() async {
     return _dio.get(
       getMesuringList,
       queryParameters: {
         'itemCode': 'PM10',
-        'dataGubun': 'HOUR',
       },
-    ).then((value) => ResponseResult.fromJson(value.data['response']));
+    ).then(
+      (value) {
+        ResponseResult responseResult =
+            ResponseResult.fromJson(value.data['response']);
+
+        Logger()
+            .i('FineDustApiClient.getAveragePM10 - ${responseResult.header}');
+
+        return responseResult;
+      },
+      onError: (error, stackTrace) {
+        Logger().e(
+          'FineDustApiClient.getAveragePM10',
+          error: error,
+          stackTrace: stackTrace,
+        );
+      },
+    );
   }
 
   Future<ResponseResult> getAveragePM25() async {
@@ -26,6 +46,23 @@ class FineDustApiClient {
         'itemCode': 'PM25',
         'dataGubun': 'HOUR',
       },
-    ).then((value) => ResponseResult.fromJson(value.data['response']));
+    ).then(
+      (value) {
+        ResponseResult responseResult =
+            ResponseResult.fromJson(value.data['response']);
+
+        Logger()
+            .i('FineDustApiClient.getAveragePM2.5 - ${responseResult.header}');
+
+        return responseResult;
+      },
+      onError: (error, stackTrace) {
+        Logger().e(
+          'FineDustApiClient.getAveragePM2.5',
+          error: error,
+          stackTrace: stackTrace,
+        );
+      },
+    );
   }
 }
