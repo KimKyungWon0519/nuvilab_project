69fc69deb556b61ed13c8c2d9e6ecd51bb71bf63
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 13:32:22 +0900

    FEAT: 미세먼지 측정 api 주소 추가

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -1,2 +1,4 @@
-String baseURL = String.fromEnvironment("BASE_URL");
-String serviceKey = String.fromEnvironment("SERVICE_KEY");
+const String baseURL = String.fromEnvironment('BASE_URL');
+const String serviceKey = String.fromEnvironment('SERVICE_KEY');
+
+const String getMesuringList = '/getCtprvnMesureLIst';
