bb1a40cad98cc18b7c3ae633e1197b34e4258652
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 22:42:45 +0900

    FEAT: 도시 별 미세먼지 가져오는 함수 추가

--- lib/data/repositoies/fine_dust_repository_impl.dart
+++ lib/data/repositoies/fine_dust_repository_impl.dart
@@ -1,4 +1,6 @@
+import 'package:nuvilab_project/data/model/response_result.dart';
 import 'package:nuvilab_project/data/service/fine_dust_api_client.dart';
+import 'package:nuvilab_project/domain/model/fine_dust_by_city.dart';
 import 'package:nuvilab_project/domain/repositoies/fine_dust_repository.dart';
 
 class FineDustRepositoryImpl implements FineDustRepository {
@@ -7,4 +9,16 @@ class FineDustRepositoryImpl implements FineDustRepository {
   FineDustRepositoryImpl({
     required FineDustApiClient fineDustApiClient,
   }) : _fineDustApiClient = fineDustApiClient;
+
+  @override
+  Future<List<FineDustByCity>> getFineDustByCities() async {
+    Future<ResponseResult> getAveragePM10 = _fineDustApiClient.getAveragePM10();
+    Future<ResponseResult> getAveragePM25 = _fineDustApiClient.getAveragePM25();
+
+    await Future.wait([getAveragePM10, getAveragePM25]).then(
+      (value) {},
+    );
+
+    return [];
+  }
 }
--- lib/domain/repositoies/fine_dust_repository.dart
+++ lib/domain/repositoies/fine_dust_repository.dart
@@ -1,1 +1,5 @@
-abstract interface class FineDustRepository {}
+import 'package:nuvilab_project/domain/model/fine_dust_by_city.dart';
+
+abstract interface class FineDustRepository {
+  Future<List<FineDustByCity>> getFineDustByCities();
+}
