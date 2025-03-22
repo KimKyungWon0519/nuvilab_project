dba4dda215cb60cf725a6c0b83340352b692aeb2
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-22 15:55:36 +0900

    FEAT: API 결과값을 ResponseReuslt로 변환

--- lib/data/service/fine_dust_api_client.dart
+++ lib/data/service/fine_dust_api_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:nuvilab_project/core/constants/api_constant.dart';
+import 'package:nuvilab_project/data/model/response_result.dart';
 
 class FineDustApiClient {
   late final Dio _dio;
@@ -8,13 +9,13 @@ class FineDustApiClient {
     required Dio dio,
   }) : _dio = dio;
 
-  getAveragePM10() async {
+  Future<ResponseResult> getAveragePM10() async {
     return _dio.get(
       getMesuringList,
       queryParameters: {
         'itemCode': 'PM10',
         'dataGubun': 'HOUR',
       },
-    );
+    ).then((value) => ResponseResult.fromJson(value.data['response']));
   }
 }
