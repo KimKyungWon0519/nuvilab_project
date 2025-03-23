ec3249d0cbadfbba9d425115e2ff87d72bbc675e
KimKyungWon0519 <gwon080870@gmail.com>
2025-03-23 10:05:29 +0900

    CHORE: 버전 충돌 해결

--- android/app/src/main/kotlin/app/kkw/nuvilab_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package app.kkw.nuvilab_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       routerConfig: AppPages.routeConfig,
     );
   }
